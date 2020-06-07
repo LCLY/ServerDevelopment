adfde2f14415cd173243da835daa2cff3bb1232f
LCLY <lchoo9294@gmail.com>
2020-06-07 16:49:03 +0800

    express simple REST

--- node-express/index.js
+++ node-express/index.js
@@ -2,15 +2,77 @@ const express = require('express');
 const http = require('http');
 // morgan let us see info on log
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
 
+// use middleware
 app.use(morgan('dev'));
 // tells express to serve static file from __dirname (root of this project), in public folder
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+
+// for all method, set status code to 200
+app.all('/dishes', (req, res, next) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  next(); //it will continue on to look for additional specification below
+});
+
+// REST API
+// after next() it will come to here below, in any of the methods, be it get, post, put or delete
+app.get('/dishes', (req, res, next) => {
+  // req and res has already passed from all
+  res.end('Will send all the dishes to you!');
+});
+
+app.post('/dishes', (req, res, next) => {
+  // req and res has already passed from all
+  // post will carry some data in the body
+  res.end(
+    'Will add the dish: ' +
+      req.body.name +
+      ' with details ' +
+      req.body.description
+  );
+});
+
+app.put('/dishes', (req, res, next) => {
+  res.statusCode = 403;
+  res.end('PUT operation not supported on /dishes');
+});
+
+app.delete('/dishes', (req, res, next) => {
+  res.end('Deleting all the dishes');
+});
+
+// REST with specific id
+app.get('/dishes/:dishId', (req, res, next) => {
+  // req and res has already passed from all
+  res.end('Will send details of the dish: ' + req.params.dishId + ' to you');
+});
+
+app.post('/dishes/:dishId', (req, res, next) => {
+  res.statusCode = 403;
+  res.end('POST operation not supported on /dishes/' + req.params.dishId);
+});
+
+app.put('/dishes/:dishId', (req, res, next) => {
+  res.write('Updating the dish ' + req.params.dishId + '\n');
+  res.end(
+    'Will update the dish: ' +
+      req.body.name +
+      ' with details: ' +
+      req.body.description
+  );
+});
+
+app.delete('/dishes/:dishId', (req, res, next) => {
+  res.end('Deleting the dish: ' + req.params.dishId);
+});
 
 app.use((req, res, next) => {
   res.statusCode = 200;
